e34d6ec794be577d89ab7ef82917770a3a3fb1d1
Evan Valencia <evanv.prof@gmail.com>
2024-02-24 10:44:53 -0500

    Parse Changes

--- DataHandling/Code/app/main.py
+++ DataHandling/Code/app/main.py
@@ -1,37 +1,61 @@
-from typing import Union
-import json
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-class Item(BaseModel):
-    recid: str
+class MissingPerson(BaseModel):
+    recid: int
     dateposted: str
     firstname: str
     lastname: str
-    currentage: str
-    datemissing: str
+    currentage: int
+    datemissing: int
     missingcity: str
     missingstate: str
     contact: str
     photolink: str
 
+class XmlSchema(BaseModel):
+    name: str
+    xml: str
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class MissingRequest(BaseModel):
+    print("Starting to parsedata")
+    XmlSchema: XmlSchema
+    xmlSchema: str
+    Row: List[dict]
+
+def parse_line(line: str) -> dict:
+    # Extracting the inner JSON string
+    inner_json_str = line.split('{"line": ')[-1].strip('",')
+
+    # Removing unnecessary characters
+    cleaned_line = inner_json_str.replace("\\", "").replace("\"", "").strip("{}")
+
+    # Splitting the cleaned data into key-value pairs
+    key_value_pairs = [pair.strip() for pair in cleaned_line.split(",")]
+
+    # Creating a dictionary from key-value pairs
+    data_dict = {}
+    for pair in key_value_pairs:
+        key, value = pair.split(":", 1)
+        data_dict[key.strip()] = value.strip()
+
+    return data_dict
+
+@app.post("/missing")
+async def missing(missing_request: MissingRequest):
+    missing_persons = []
+
+    for row in missing_request.Row:
+        # Extracting and parsing the "line" field
+        data_dict = parse_line(row["line"])
+        print("Finished parsing data")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+        # Creating a MissingPerson object
+        missing_person = MissingPerson(**data_dict)
+        missing_persons.append(missing_person)
 
-@app.post("/addData/")
-async def addData(item: Item):
-    # Save the data to a file
-    with open('DataHandling/Code/app', 'w') as file:
-        json.dump(item.model_dump(), file)
-        file.write('\n')
-    
-    return item
+    return {"missing_persons": missing_persons}
